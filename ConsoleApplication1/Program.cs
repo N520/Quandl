0462754cfb8f09eaf16482a738daf2c498d30335
Rainer <Rainermeindl@gmail.com>
2016-12-30 15:31:30 +0100

    implemented parallel version without async

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -9,12 +9,20 @@ namespace ConsoleApplication1 {
         private static string[] names = { "NASDAQ_MSFT", "NASDAQ_AAPL", "NASDAQ_GOOG" };
 
         static void Main(string[] args) {
+            List<Task<string>> list = new List<Task<string>>();
             foreach (var name in names) {
-               
+                var task = Task.Run(() => RetrieveStockData(name));
+                var seriesTask = Task.Run(() => GetSeries(new List<string>() { task.Result }, name));
+                var trendTask = Task.Run(() => GetTrend(new List<string>() { task.Result }, name));
+                list.Add(seriesTask); list.Add(trendTask);
             }
+            Task.WaitAll(list.ToArray());
+            Console.WriteLine("Done");
+            DisplayData(list.Select(item => item.Result).ToList());
         }
 
         private static string RetrieveStockData(string name) {
+            Console.WriteLine($"{name} stockValues");
             return $"{name} stockValues";
         }
 
@@ -23,6 +31,7 @@ namespace ConsoleApplication1 {
             foreach (var value in stockValues) {
                 s += $" {value}";
             }
+            Console.WriteLine(s + " series");
             return s + " series";
         }
 
@@ -31,7 +40,14 @@ namespace ConsoleApplication1 {
             foreach (var value in stockValues) {
                 s += $" {value}";
             }
+            Console.WriteLine(s + " trend");
             return s + " trend";
+        }
+
+        private static void DisplayData(List<string> seriesList) {
+            foreach (var item in seriesList) {
+                Console.WriteLine(item);
+            }
         }
     }
 }
